9b962f5aa2b68be62f365726156b1b6ff3f87701
reteyery <729239206@qq.com>
2020-04-23 17:55:54 +0800

    Add Login page

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,30 +1,168 @@
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class LoginDart extends StatelessWidget{
+class LoginDart extends StatefulWidget {
+  LoginDart({Key key, this.title}) : super(key: key);
+
+  final String title;
 
   @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: new AppBar(
-          title: new Text('this is login page'),
-          backgroundColor: Colors.green,
-        ),
-        body: new Center(
-          child: new RaisedButton(
-              onPressed: (){
-                Navigator.pop(context);
-              } ,
-              child: new Text(
-                  'back',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 16
-              ),),
-          color: Colors.blue,), //RaiseButton背景色
-        ),
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginDart> {
+
+  Widget _entryField(String title, {bool isPassword = false}) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            title,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextField(
+              obscureText: isPassword,
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  fillColor: Color(0xfff3f3f4),
+                  filled: true))
+        ],
+      ),
     );
   }
 
+  Widget _submitButton() {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.symmetric(vertical: 15),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+                color: Colors.grey.shade200,
+                offset: Offset(2, 4),
+                blurRadius: 5,
+                spreadRadius: 2)
+          ],
+          gradient: LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: [Colors.greenAccent, Colors.green])),
+      child: Text(
+        '登录',
+        style: TextStyle(fontSize: 20, color: Colors.white),
+      ),
+    );
+  }
+
+//  Widget _divider() {
+//    return Container(
+//      margin: EdgeInsets.symmetric(vertical: 10),
+//      child: Row(
+//        children: <Widget>[
+//          SizedBox(
+//            width: 20,
+//          ),
+//          Expanded(
+//            child: Padding(
+//              padding: EdgeInsets.symmetric(horizontal: 10),
+//              child: Divider(
+//                thickness: 1,
+//              ),
+//            ),
+//          ),
+//          Text('or'),
+//          Expanded(
+//            child: Padding(
+//              padding: EdgeInsets.symmetric(horizontal: 10),
+//              child: Divider(
+//                thickness: 1,
+//              ),
+//            ),
+//          ),
+//          SizedBox(
+//            width: 20,
+//          ),
+//        ],
+//      ),
+//    );
+//  }
+
+  Widget _title() {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+          text: 'DFSYNC',
+          style: GoogleFonts.portLligatSans(
+            textStyle: Theme.of(context).textTheme.display1,
+            fontSize: 30,
+            fontWeight: FontWeight.w700,
+            color: Colors.green,
+          ),
+      ),
+    );
+  }
+
+  Widget _emailPasswordWidget() {
+    return Column(
+      children: <Widget>[
+        _entryField("账号"),
+        _entryField("密码", isPassword: true),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Expanded(
+                          flex: 3,
+                          child: SizedBox(),
+                        ),
+                        _title(),
+                        SizedBox(
+                          height: 50,
+                        ),
+                        _emailPasswordWidget(),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        _submitButton(),
+                        Container(
+                          padding: EdgeInsets.symmetric(vertical: 10),
+                          alignment: Alignment.centerRight,
+                          child: Text('忘记密码 ?',
+                              style:
+                              TextStyle(fontSize: 14, fontWeight: FontWeight.w500)),
+                        ),
+                        Expanded(
+                          flex: 2,
+                          child: SizedBox(),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+        )
+    );
+  }
 }
