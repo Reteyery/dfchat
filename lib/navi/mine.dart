e6f2ce0eb574bde26aeedd37c8f7d1de016a57ec
reteyery <729239206@qq.com>
2020-04-26 10:12:09 +0800

    Add setting detail

--- lib/navi/mine.dart
+++ lib/navi/mine.dart
@@ -16,7 +16,7 @@ class _MinePageState extends State<MineDart>{
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,//隐藏返回按钮
-        title: Text("DFSYNC",
+        title: Text("设置",
         style: TextStyle(
           color: Const.C_GREEN,
 //          fontWeight: FontWeight.bold,
@@ -47,19 +47,116 @@ class _MinePageState extends State<MineDart>{
               ),
             ),
             new Container(
+                height: 60,
+                width: MediaQuery.of(context).size.width,
+                color: Colors.white,
+                margin: EdgeInsets.only(top:0.5),
+                padding: EdgeInsets.only(left: 15),
+                alignment: Alignment.centerLeft,
+                child: new Row(
+                  children: <Widget>[
+                    new Icon(Icons.settings, size: 23, color: Const.C_GREEN),
+                    Padding(padding: EdgeInsets.only(left: 12)),
+                    new Text("按回车键发送消息",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black54,
+                      ),),
+                    new Switch(value: true,
+                      onChanged: null,
+                      inactiveThumbColor: Colors.grey,
+                      activeColor: Const.C_GREEN,
+                      activeTrackColor: Const.C_GREEN
+                    )
+                  ],
+                )
+            ),
+            new Container(
+                height: 60,
+                width: MediaQuery.of(context).size.width,
+                color: Colors.white,
+                margin: EdgeInsets.only(top:0.5),
+                padding: EdgeInsets.only(left: 15),
+                alignment: Alignment.centerLeft,
+                child: new Row(
+                  children: <Widget>[
+                    new Icon(Icons.settings, size: 23, color: Const.C_GREEN),
+                    Padding(padding: EdgeInsets.only(left: 12)),
+                    new Text("允许多端同时在线",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black54,
+                      ),),
+                    new Switch(value: false,
+                      onChanged: null,
+                      inactiveThumbColor: Colors.grey,
+                      activeColor: Const.C_GREEN,
+                      activeTrackColor: Const.C_GREEN
+                    )
+                  ],
+                )
+            ),
+            new Container(
               height: 60,
               width: MediaQuery.of(context).size.width,
               color: Colors.white,
               margin: EdgeInsets.only(top:0.5),
               padding: EdgeInsets.only(left: 15),
               alignment: Alignment.centerLeft,
-              child: new Text("设置",
-                textAlign: TextAlign.start,
-                style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.black54,
-                    fontWeight: FontWeight.bold
-                ),),
+                child: new Row(
+                  children: <Widget>[
+                    new Icon(Icons.mode_edit, size: 23, color: Const.C_GREEN),
+                    Padding(padding: EdgeInsets.only(left: 12)),
+                    new Text("修改密码",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black54,
+                      ),),
+                  ],
+                )
+            ),
+            new Container(
+              height: 60,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.white,
+              margin: EdgeInsets.only(top:0.5),
+              padding: EdgeInsets.only(left: 15),
+              alignment: Alignment.centerLeft,
+                child: new Row(
+                  children: <Widget>[
+                    new Icon(Icons.favorite, size: 23, color: Const.C_GREEN),
+                    Padding(padding: EdgeInsets.only(left: 12)),
+                    new Text("关于我们",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black54,
+                      ),),
+                  ],
+                )
+            ),
+            new Container(
+              height: 60,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.white,
+              margin: EdgeInsets.only(top:0.5),
+              padding: EdgeInsets.only(left: 15),
+              alignment: Alignment.centerLeft,
+              child: new Row(
+                children: <Widget>[
+                  new Icon(Icons.exit_to_app, size: 23, color: Const.C_GREEN),
+                  Padding(padding: EdgeInsets.only(left: 12)),
+                  new Text("安全退出",
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black54,
+                    ),),
+                ],
+              )
             ),
           ],
         ),
